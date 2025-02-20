5660d7571d54fbb126d2ecc77db976c11b6db469
Emilien Bidet <emilienbdt@gmail.com>
2025-02-20 09:26:10 +0100

    Add js doc for HTTPMethod

--- packages/fumi/src/router/http-method.ts
+++ packages/fumi/src/router/http-method.ts
@@ -1,1 +1,6 @@
+/**
+ * Represents the HTTP methods supported by the server.
+ *
+ * @description Defines the allowed HTTP methods for routing requests.
+ */
 export type HTTPMethod = "GET" | "POST";
